e178cbe7b32196c1c74f4c92985ea5de2f3a59f9
ToDarkClown <294203341@qq.com>
2017-07-12 08:29:20 +0800

    OK

---
+++ RNT/Controllers/LARController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RNT.Controllers
+{
+    public class LARController : Controller
+    {
+        // GET: LAR
+        /// <summary>
+        /// Login And Register
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
